cc95663ab14768f44153444b88f740f372c5ee85
Aly <Aly.jalil@icloud.com>
2024-04-20 16:52:12 +0200

    test

--- src/ifc_processor.py
+++ src/ifc_processor.py
@@ -9,8 +9,8 @@ def process_ifc_file(ifc_file_path):
     num_vertex_occurrences = {}
     for product in ifc_file.by_type("IfcProduct"):
         
-        new_shape = ifcopenshell.geom.create_shape(settings, product)
-        num_vertex = new_shape.geometry.verts
+        shape = ifcopenshell.geom.create_shape(settings, product)
+        num_vertex = shape.geometry.verts
         num_vertex_occurrences[product.Name] = num_vertex
     
     return num_vertex_occurrences
