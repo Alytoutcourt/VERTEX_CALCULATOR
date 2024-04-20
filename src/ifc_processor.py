198c4ea3e7c50b749c1629d3665715fede739472
Aly <Aly.jalil@icloud.com>
2024-04-20 16:41:41 +0200

    test

--- src/ifc_processor.py
+++ src/ifc_processor.py
@@ -3,7 +3,7 @@ import ifcopenshell
 def process_ifc_file(ifc_file_path):
     ifc_file = ifcopenshell.open(ifc_file_path)
     
-    return {ifc_file.name : "TEST"}
+    return {ifc_file.schema : "TEST"}
     # num_vertex_occurrences = {}
     # for product in ifc_file.by_type("IfcProduct"):
     #     num_vertex = len(product.Representation.Representations[0].Items[0].Coordinates)
