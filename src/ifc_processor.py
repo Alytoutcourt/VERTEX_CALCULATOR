0429d3de5b2ad6fbd997d0eb457d7a446249fb4b
Aly <Aly.jalil@icloud.com>
2024-04-20 16:54:52 +0200

    test

--- src/ifc_processor.py
+++ src/ifc_processor.py
@@ -1,16 +1,25 @@
 import ifcopenshell
-import ifcopenshell.geom
-import ifcopenshell.util.shape
+import multiprocessing
 
 def process_ifc_file(ifc_file_path):
     ifc_file = ifcopenshell.open(ifc_file_path)
     settings = ifcopenshell.geom.settings()
-    # return {ifc_file.schema : "TEST"}
-    num_vertex_occurrences = {}
-    for product in ifc_file.by_type("IfcProduct"):
-        
-        shape = ifcopenshell.geom.create_shape(settings, product)
-        num_vertex = shape.geometry.verts
-        num_vertex_occurrences[product.Name] = num_vertex
-    
-    return num_vertex_occurrences
+    iterator = ifcopenshell.geom.iterator(settings, ifc_file, multiprocessing.cpu_count())
+    if iterator.initialize():
+        num_vertex_occurrences = {}
+        while True:
+            shape = iterator.get()
+            if shape is None:
+                break
+            product_name = shape.product.Name
+            num_faces = len(shape.geometry.faces)
+            num_edges = len(shape.geometry.edges)
+            num_verts = len(shape.geometry.verts)
+            num_vertex_occurrences[product_name] = {
+                "num_faces": num_faces,
+                "num_edges": num_edges,
+                "num_verts": num_verts
+            }
+            if not iterator.next():
+                break
+        return num_vertex_occurrences
