11a9dc41d6da7baa693a58e85b3205da64ec1360
Aly <Aly.jalil@icloud.com>
2024-04-20 15:38:40 +0200

    test

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import os  # Importation du module os pour manipuler les chemins de fichiers
-import tempfile  # Importation du module tempfile pour créer un répertoire temporaire
 import streamlit as st  # Importation du module streamlit pour créer l'interface utilisateur
 from ifc_processor import process_ifc_file  # Importation de la fonction process_ifc_file du module ifc_processor
 
@@ -7,32 +6,26 @@ def main():
     # Titre de l'application
     st.title("IFC Vertex Calculator")
     
-    # Uploader pour télécharger un fichier IFC
-    uploaded_file = st.file_uploader("Upload an IFC file", type="ifc")
+    # Champ de saisie de texte pour entrer le chemin du fichier IFC
+    ifc_path = st.text_input("Enter the path of the IFC file:")
     
-    # Vérifie si un fichier a été téléchargé
-    if uploaded_file is not None:
+    # Vérifie si un chemin a été saisi
+    if ifc_path:
         # Crée un bouton pour démarrer le calcul des vertex
         if st.button("Calculate Vertex"):
-            # Crée un répertoire temporaire pour stocker le fichier IFC
-            temp_dir = tempfile.mkdtemp()
-            
-            # Obtient le chemin du fichier IFC dans le répertoire temporaire
-            path = os.path.join(temp_dir, uploaded_file.name)
-            
-            # Écrit le contenu du fichier IFC dans le fichier temporaire
-            with open(path, "wb") as f:
-                f.write(uploaded_file.getvalue())
-            
-            # Appelle la fonction pour traiter le fichier IFC et obtenir les occurrences de vertex
-            num_vertex_occurrences = process_ifc_file(path)
-            
-            # Affiche les résultats dans l'interface utilisateur
-            st.write("### Vertex Occurrences:")
-            st.write("| Occurrence | Number of Vertex |")
-            st.write("| --- | --- |")
-            for occurrence, num_vertex in num_vertex_occurrences.items():
-                st.write(f"| {occurrence} | {num_vertex} |")
+            # Vérifie si le fichier IFC existe à l'emplacement spécifié
+            if os.path.exists(ifc_path):
+                # Appelle la fonction pour traiter le fichier IFC et obtenir les occurrences de vertex
+                num_vertex_occurrences = process_ifc_file(ifc_path)
+                
+                # Affiche les résultats dans l'interface utilisateur
+                st.write("### Vertex Occurrences:")
+                st.write("| Occurrence | Number of Vertex |")
+                st.write("| --- | --- |")
+                for occurrence, num_vertex in num_vertex_occurrences.items():
+                    st.write(f"| {occurrence} | {num_vertex} |")
+            else:
+                st.error("File not found. Please enter a valid path to the IFC file.")
 
 if __name__ == "__main__":
     main()
